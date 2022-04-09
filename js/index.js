a4668d38234c7deb6d31e402f219753da78461b5
Minh <dackhers@gmail.com>
2022-04-09 22:46:02 +0700

    Add increment animation

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
+    let rs
+
     $.ajax({url: 'https://api.covid19api.com/dayone/country/viet-nam', async: false, success: function(result){
-        
-        const rs = result[result.length-1]
+        rs = result[result.length-1]
         $('#country .card__value').text(rs.Country)
-        $('#recovered .card__value').text(rs.Deaths)
-        $('#confirmed .card__value').text(rs.Confirmed)
-        $('#death .card__value').text(rs.Deaths)
-        $('#active .card__value').text(rs.Active)
         $('#time .card__value').text(new Date(rs.Date).toDateString())
     }})
     
+    let recoveredTimers = setInterval(()=>increment($('#recovered .card__value'), rs.Recovered, recoveredTimers))
+    let deathTimers = setInterval(()=>increment($('#death .card__value'), rs.Deaths, deathTimers))
+    let confirmedTimers = setInterval(()=>increment($('#confirmed .card__value'), rs.Confirmed, confirmedTimers))
+    let activeTimers = setInterval(()=>increment($('#active .card__value'), rs.Active, activeTimers))
+
+
+    function increment (elem, prop, timers) {
+        const step = Math.round(prop / 1000)    
+        let value = elem.text()
+        elem.text(- -value  + step)
+        value >= prop && clearInterval(timers)
+    }
 })
